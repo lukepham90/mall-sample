6e62e5daac608a5be07f9245f65a0423191aac7f
uuhnaut69 <uuhnaut96@gmail.com>
2020-02-03 22:31:54 +0700

    # Init project

---
+++ security-service/src/main/java/com/uuhnaut69/mall/security/user/CurrentUser.java
@@ -0,0 +1,13 @@
+package com.uuhnaut69.mall.security.user;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
